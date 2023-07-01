4dbde02dd825e80c405f251694826c1b38f0a442
cgmossa <cgmossa@gmail.com>
2023-07-01 15:21:10 +0200

    Added `export_bindings` feature

--- build.rs
+++ build.rs
@@ -1,5 +1,9 @@
 use std::process::Command;
 
+/// Returns the result of running `R CMD config [flag]`.
+///
+/// Panics if R isn't available in `PATH`.
+///
 pub fn run_r_cmd_config(flag: &str) -> String {
     let r_cmd_config_flag = Command::new("R")
         .args(["CMD", "config", flag])
@@ -16,6 +20,43 @@ fn main() {
     println!("cargo:rustc-link-search=libgcc_mock");
     let cppflags = run_r_cmd_config("--cppflags");
 
+    #[cfg(feature = "export_bindings")]
+    export_bindings(&cppflags);
+
+    let ldflags = run_r_cmd_config("--ldflags");
+    dbg!(&ldflags);
+
+    let mut wk_build = cc::Build::new();
+    let mut wk_build = wk_build
+        .cargo_metadata(true)
+        .file("wk/inst/include/wk-v1-impl.c");
+    for flag in cppflags
+        .split_ascii_whitespace()
+        .chain(ldflags.split_ascii_whitespace())
+    {
+        println!("cargo:rustc-link-arg={}", flag);
+        wk_build = wk_build.flag(flag);
+    }
+
+    let cxx23flags = run_r_cmd_config("CXX23FLAGS");
+    dbg!(&cxx23flags);
+    for flag in cxx23flags.split_ascii_whitespace() {
+        wk_build = wk_build.flag(flag);
+    }
+
+    wk_build
+        // -s, --strip-all             Strip all symbols
+        .flag("-s")
+        .debug(false)
+        // warning that isn't useful
+        .flag("-Wno-unused-parameter")
+        .extra_warnings(true)
+        .shared_flag(true)
+        .compile("wk");
+}
+
+#[cfg(feature = "export_bindings")]
+fn export_bindings(cppflags: &String) {
     let r_tools_soft = run_r_cmd_config("R_TOOLS_SOFT");
 
     dbg!(&cppflags, &r_tools_soft);
@@ -40,7 +81,7 @@ fn main() {
         .sort_semantically(true)
         .layout_tests(false)
         // .default_macro_constant_type(bindgen::MacroTypeVariation::Signed)
-        
+
         .wrap_unsafe_ops(true)
 
         .clang_args(cppflags.split_ascii_whitespace())
@@ -90,35 +131,4 @@ fn main() {
     //     .unwrap()
     //     .write_to_file("src/bindings_wk_filter.rs")
     //     .unwrap();
-
-    let ldflags = run_r_cmd_config("--ldflags");
-    dbg!(&ldflags);
-
-    let mut wk_build = cc::Build::new();
-    let mut wk_build = wk_build
-        .cargo_metadata(true)
-        .file("wk/inst/include/wk-v1-impl.c");
-    for flag in cppflags
-        .split_ascii_whitespace()
-        .chain(ldflags.split_ascii_whitespace())
-    {
-        println!("cargo:rustc-link-arg={}", flag);
-        wk_build = wk_build.flag(flag);
-    }
-
-    let cxx23flags = run_r_cmd_config("CXX23FLAGS");
-    dbg!(&cxx23flags);
-    for flag in cxx23flags.split_ascii_whitespace() {
-        wk_build = wk_build.flag(flag);
-    }
-
-    wk_build
-        // -s, --strip-all             Strip all symbols
-        .flag("-s")
-        .debug(false)
-        // warning that isn't useful
-        .flag("-Wno-unused-parameter")
-        .extra_warnings(true)
-        .shared_flag(true)
-        .compile("wk");
 }
